429b9d626851818d3b7884fc57bafbe696d4bdc3
Marcela Mariotti Peres <marcelamperes@gmail.com>
2017-01-04 15:36:42 -0200

    adicionando versão teste - funcionando

---
+++ poc/client/Program.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using Services.Poc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel("127.0.0.1:5000", ChannelCredentials.Insecure);
+            var client = new Logging.LoggingClient(channel);
+
+            // YOUR CODE GOES HERE
+            MessageRequest request = new MessageRequest() { Message = "teste" };
+            MessageResponse message = client.SendMessage(request);
+
+            Console.WriteLine(message.Message);
+            Console.ReadKey();
+
+            channel.ShutdownAsync().Wait();
+
+
+        }
+    }
+}
